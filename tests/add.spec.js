be114dc3106fc52e4a8f0ee17da1c9ee13bc0746
David Jenkins <davidjenkins479@gmail.com>
2025-01-17 21:07:17 +0100

    Solved lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,24 +1,30 @@
-describe("Iteration 1 | Getting Started", () => {
-    describe("Function add", () => {
-      it("should be defined", () => {
-        expect(add).toBeDefined();
-      });
-  
-      it("should take two arguments", () => {
-        expect(add.length).toBe(2);
-      });
-  
-      it("should return the sum of the two numbers", () => {
-        expect(add(1, 2)).toEqual(3);
-        expect(add(3, 4)).toEqual(7);
-        expect(add(100, 47)).toEqual(147);
-      });
-  
-      it("should return undefined if any of the arguments is not provided", () => {
-        expect(add(1)).toEqual(undefined);
-        expect(add()).toEqual(undefined);
-        expect(add(undefined, 1)).toEqual(undefined);
-      });
-    });
+// describe("Iteration 1 | Getting Started", () => {
+describe("Function add", () => {
+  //     it("should be defined", () => {
+  //       expect(add).toBeDefined();
+  //     });
+
+  //     it("should take two arguments", () => {
+  //       expect(add.length).toBe(2);
+  //     });
+
+  //     it("should return the sum of the two numbers", () => {
+  //       expect(add(1, 2)).toEqual(3);
+  //       expect(add(3, 4)).toEqual(7);
+  //       expect(add(100, 47)).toEqual(147);
+  //     });
+
+  //     it("should return undefined if any of the arguments is not provided", () => {
+  //       expect(add(1)).toEqual(undefined);
+  //       expect(add()).toEqual(undefined);
+  //       expect(add(undefined, 1)).toEqual(undefined);
+  //     });
+  //   });
+  // });
+
+  it("should return undefined if any of the two arguments", function () {
+    expect(add(1, null)).toEqual(undefined);
+    expect(add({}, "15")).toEqual(undefined);
+    expect(add("10", "69")).toEqual(undefined);
   });
-  
+});
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,26 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+  describe("Function - calculateArea", () => {
+    it("should be defined", () => {
+      expect(calculateArea.length).toBe(2);
+    });
 
-    })    
-})
+    it("should take two numbers as arguments", () => {
+      expect(calculateArea(1, 2)).toEqual(2);
+      expect(calculateArea(5, 10)).toEqual(50);
+    });
 
-*/
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(calculateArea(1)).toEqual(undefined);
+      expect(calculateArea()).toEqual(undefined);
+      expect(calculateArea(undefined, 1)).toEqual(undefined);
+    });
+
+    it("should return undefined if any of the argument is not a number", () => {
+      expect(calculateArea(null, "1")).toEqual(undefined);
+      expect(calculateArea(true, "1")).toEqual(undefined);
+      expect(calculateArea(10, "5")).toEqual(undefined);
+    });
+  });
+});
